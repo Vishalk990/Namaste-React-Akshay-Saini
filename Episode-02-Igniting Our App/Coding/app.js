38bb0caa240c11800a999099eed3b15875e1fdf1
Vishal Kalita <101493981+Vishalk990@users.noreply.github.com>
2023-09-28 19:45:37 +0530

    Episode - 02 - Igniting Our App

---
+++ Episode-02-Igniting Our App/Coding/app.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+// ReactElement(Object) => HTMLAllCollection(Browser Understands)
+
+
+const parent = React.createElement(
+    "div", { id: "parent" },
+    [React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I am an h1 Tag."), React.createElement("h2", {}, "H2 tag."),
+    ]),
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I am an h1 Tag."), React.createElement("h2", {}, "I am an h2 Tag."),
+    ])
+    ]);
+
+//JSX
+
+console.log(parent);  //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
