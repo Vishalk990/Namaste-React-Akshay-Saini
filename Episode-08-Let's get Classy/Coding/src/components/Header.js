ff8b51e71028d6d4bdbdfe0d822ef920d402f124
Vishal Kalita <vishalkalita036@gmail.com>
2024-06-06 01:54:33 +0530

    Episode-08 Completed

---
+++ Episode-08-Let's get Classy/Coding/src/components/Header.js
@@ -0,0 +1,37 @@
+import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+
+  const [btnName, setBtnName] = useState("Login");
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact us</Link>
+          </li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login")
+            }}>{btnName}</button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
